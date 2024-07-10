95babe05b5010542347e4f516d21fb90f1dffa79
garrettladley <garrett.ladley14@gmail.com>
2024-07-09 21:51:28 -0400

    done

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"movie-matcher/internal/config"
+	"movie-matcher/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	settings, err := config.GetSettings("config")
+	if err != nil {
+		panic(err)
+	}
+
+	app := server.Setup(*settings)
+
+	go func() {
+		if err := app.Listen(strconv.Itoa(int(settings.Application.Port))); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	slog.Info("Shutting down server")
+	if err := app.Shutdown(); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("Server shutdown")
 }
