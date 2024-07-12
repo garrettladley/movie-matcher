78570bcec0d648b1e534967b7ec05617d92dd1b4
garrettladley <garrett.ladley14@gmail.com>
2024-07-11 21:58:37 -0400

    feat: use new types in preference

--- internal/model/person.go
+++ internal/model/person.go
@@ -5,8 +5,8 @@ import "time"
 type Person struct {
 	Name           string                    `json:"name"`
 	Runtime        Preference[time.Duration] `json:"runtime,omitempty"`
-	Language       Preference[string]        `json:"language,omitempty"`
-	Actor          Preference[string]        `json:"actor,omitempty"`
+	Languages      Preference[[]Language]    `json:"languages,omitempty"`
+	Actors         Preference[[]string]      `json:"actors,omitempty"`
 	RottenTomatoes Preference[int]           `json:"rotten_tomatoes,omitempty"`
 	Year           Preference[int]           `json:"year,omitempty"`
 	Rating         Preference[string]        `json:"rating,omitempty"`
