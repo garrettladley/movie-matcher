1381cd6e748e6dde9c992540747a8950c1440179
Alder Whiteford <alderjwhiteford@gmail.com>
2024-07-24 22:27:06 -0400

    Minor URL Fix (#35)

--- internal/movie/catalog.go
+++ internal/movie/catalog.go
@@ -46,19 +46,19 @@ var ContinueWatchingCatalog = ordered_set.New[ID](
 )
 
 var PosterCatalog = map[ID]string{
-	"tt2582802":  "https://i.imgur.com/PlNisIp.jpeg", // whiplash
-	"tt15239678": "https://i.imgur.com/UQHlMQ1.jpeg", // dune 2
-	"tt3783958":  "https://i.imgur.com/8YmryZL.jpeg", // lalaland
-	"tt1490017":  "https://i.imgur.com/nuyD2fN.jpeg", // the lego movie
-	"tt0062622":  "https://i.imgur.com/cqEE5PG.jpeg", // 2001: a space odyssey
-	"tt22022452": "https://i.imgur.com/nvzrkkz.jpeg", // inside out 2
-	"tt2278388":  "https://i.imgur.com/1mM13lC.jpeg", // the grand budapest hotel
-	"tt2084970":  "https://i.imgur.com/cKz9dhr.jpeg", // the imitation game
-	"tt0112384":  "https://i.imgur.com/DIdS6mp.jpeg", // apollo 13
-	"tt0264464":  "https://i.imgur.com/qGqain2.jpeg", // catch me if you can
-	"tt0432283":  "https://i.imgur.com/PDpAJLK.jpeg", // fantastic mr fox
-	"tt2293640":  "https://i.imgur.com/pPkUfRP.jpeg", // the minions movie
-	"tt0058150":  "https://i.imgur.com/VWKIDR0.jpeg", // goldfinger
-	"tt1074638":  "https://i.imgur.com/XHeEjqy.jpeg", // skyfall
-	"tt1285016":  "https://i.imgur.com/q0WxmVj.jpeg", // the social network
+	"tt2582802":  "https://i.imgur.com/PlNisIp.jpg", // whiplash
+	"tt15239678": "https://i.imgur.com/UQHlMQ1.jpg", // dune 2
+	"tt3783958":  "https://i.imgur.com/8YmryZL.jpg", // lalaland
+	"tt1490017":  "https://i.imgur.com/nuyD2fN.jpg", // the lego movie
+	"tt0062622":  "https://i.imgur.com/cqEE5PG.jpg", // 2001: a space odyssey
+	"tt22022452": "https://i.imgur.com/nvzrkkz.jpg", // inside out 2
+	"tt2278388":  "https://i.imgur.com/1mM13lC.jpg", // the grand budapest hotel
+	"tt2084970":  "https://i.imgur.com/cKz9dhr.jpg", // the imitation game
+	"tt0112384":  "https://i.imgur.com/DIdS6mp.jpg", // apollo 13
+	"tt0264464":  "https://i.imgur.com/qGqain2.jpg", // catch me if you can
+	"tt0432283":  "https://i.imgur.com/PDpAJLK.jpg", // fantastic mr fox
+	"tt2293640":  "https://i.imgur.com/pPkUfRP.jpg", // the minions movie
+	"tt0058150":  "https://i.imgur.com/VWKIDR0.jpg", // goldfinger
+	"tt1074638":  "https://i.imgur.com/XHeEjqy.jpg", // skyfall
+	"tt1285016":  "https://i.imgur.com/q0WxmVj.jpg", // the social network
 }
