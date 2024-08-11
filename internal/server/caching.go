105b2db0705484f8fd1302c1acf0b791eaefca72
garrettladley <garrett.ladley14@gmail.com>
2024-08-10 21:23:56 -0400

    fast follow: images & update paths

--- internal/server/caching.go
+++ internal/server/caching.go
@@ -1,9 +1,9 @@
 package server
 
 var StaticPaths = map[string]struct{}{
-	"/deps/apexcharts.min.js":            {},
-	"/deps/htmx.min.js":                  {},
-	"/deps/flowbite.min.js":              {},
-	"/public/styles.css":                 {},
-	"/images/dark_backgroundkground.jpg": {},
+	"/deps/apexcharts.min.js":     {},
+	"/deps/htmx.min.js":           {},
+	"/deps/flowbite.min.js":       {},
+	"/public/styles.css":          {},
+	"/images/dark_background.jpg": {},
 }
